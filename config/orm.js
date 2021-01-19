35faeea0cc0fc59ceddfa982b51a4bbd282f5fb7
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-01-19 17:37:28 -0500

    initial setup, creating more directories

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,8 @@
 // Establish connection to database
 const connection = require("connection");
 
-// Methods that will execute the necessary MySQL commands in the controllers
+// Methods that will execute the necessary MySQL commands in the controllers.
+// These are the methods you will need to use in order to retrieve and store data in your database.
 
 function selectAll() {
 
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const burger = require("burger");
+
+// Create the `router` for the app, and export the `router` at the end of your file.
+
+module.exports = burgers_controller;

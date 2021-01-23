680fd3ef9016dfb3f59df7bb946b414370994cc1
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-01-23 18:16:36 -0500

    added burgers controller file, router completed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,43 @@
-const express = require("express");
-const burger = require("burger");
+// LOAD DEPENDENCIES
+var express = require('express');
+var router = express.Router();
 
-// Create the `router` for the app, and export the `router` at the end of your file.
+// Import burger model to access burger db functions
+var burger = require('../models/burger.js');
 
-module.exports = burgers_controller;
+// Create routes
+// get all burgers route
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+// add burger route
+router.post('/api/burgers', (req, res) => {
+    burger.addOne(['burger_name'],
+     [
+        req.body.burger_name
+     ], (data) => {
+        // redirect to origin page, new burger will display
+        res.redirect('/');
+    });
+});
+
+// update burger route
+router.put('/api/burgers/:id', (req, res) => {
+    var condition = 'id = ' + req.params.id;
+
+    burger.updateOne({
+        devoured: true
+    }, condition, (data) => {
+        // redirect to origin page, burger devour state will update
+        res.redirect('/');
+    });
+});
+
+module.exports = router;

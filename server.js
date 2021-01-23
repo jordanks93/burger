1ca0b6af1f0a028a4531e2e05f83562a1ca03ce7
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-01-23 18:22:05 -0500

    updated server file

--- server.js
+++ server.js
@@ -1,2 +1,34 @@
 // LOAD DEPENDENCIES 
-const express = require("express");
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Allows Override of POST method in index.handlebars 
+app.use(methodOverride('_method'));
+
+// Set Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// allows handlebars to access image file
+app.use(express.static('public/assets/img'));
+
+// Import routes and give the server access
+var routes = require("./controllers/burgers_Controller.js");
+app.use(routes);
+
+app.listen(PORT, function () {
+  console.log("App now listening at localhost:" + PORT);
+});

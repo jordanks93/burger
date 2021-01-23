2c846e4ca10bd874463edb75eb04a794ab4983b2
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-01-23 18:20:02 -0500

    updated burger model

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,29 @@
-const orm = require("orm");
+// Import ORM with functions that make it easier to interact with db
+var orm = require('../config/orm.js');
 
-// create the code that will call the ORM functions using burger specific input for the ORM.
+// Burger object with functions to interact with burgers db
+var burger = {
 
+  // Select all burgers in the db
+  selectAll: function(cb) {
+    orm.selectAll('burgers', (res) => {
+      cb(res);
+    });
+  },
+
+  // Add a burger to the table
+  addOne: function(cols, vals, cb) {
+    orm.addOne('burgers', cols, vals, (res) => {
+      cb(res);
+    });
+  },
+
+  // Update a burger in the table, used to update devoured state
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, (res) => {
+      cb(res);
+    });
+  }
+};
 
 module.exports = burger;

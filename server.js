aa5a80e55a72eab5110e5e71852e167cf4f98841
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2021-01-25 16:47:08 -0500

    heroku troubleshooting

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static('public/assets/img'));
 
 // Import routes and give the server access
 var routes = require("../controllers/burgers_controller.js");
-app.use(routes);
+app.use('/' + routes);
 
 app.listen(PORT, function () {
   console.log("App now listening at localhost:" + PORT);
